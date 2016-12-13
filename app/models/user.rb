623664e6c0b55357ffc3ffeaf75e9a60406e7c3b
javkhlantugs <jay.mudgil@gmail.com>
2016-12-13 11:56:36 -0500

    paperclip file size limit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   
   has_attached_file :avatar, styles: { medium: "400x400>", thumb: "100x100#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  validates_attachment_size :image, :in => 0.megabytes..2.megabytes
+
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
