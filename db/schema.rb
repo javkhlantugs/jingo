4ce519a220e03741fa7cecf04ae2fb1b6d8de86d
javkhlantugs <jay.mudgil@gmail.com>
2017-01-05 03:40:41 -0500

    removed unecessary sign up stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161212171631) do
+ActiveRecord::Schema.define(version: 20161231033911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,8 +49,6 @@ ActiveRecord::Schema.define(version: 20161212171631) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "hometown"
-    t.string   "phone"
-    t.integer  "age"
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
